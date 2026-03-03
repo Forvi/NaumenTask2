3bdae548249718b7b4378c1911a454083a9611ca
Forvi <nforvi@mail.ru>
2026-03-03 22:33:28 +0500

    Перенес пример выполнения в App.java

--- app/src/main/java/org/example/ArraysComparator.java
+++ app/src/main/java/org/example/ArraysComparator.java
@@ -23,19 +23,5 @@ public class ArraysComparator<T extends Number> {
                 .stream(arr)
                 .max(Comparator.comparing(e -> Math.abs(e.doubleValue())));
     }
-
-    public static void main(String[] args) {
-        // p.s. Добавил отрицательные значения, несмотря на условие [0, ∞),
-        // т.к. суть задания - найти АБСОЛЮТНОЕ значение
-        Integer[] numbers = { -1, -23, 1, 4, 6, 7, 15 };
-        System.out.println("Исходный массив: " + Arrays.toString(numbers));
-
-        var arrayComparator = new ArraysComparator<>(numbers);
-        Optional<Integer> result = arrayComparator.findAbsMax();
-        result.ifPresentOrElse(
-                v ->
-                        System.out.println("Максимальное значение по модулю: " + Math.abs(v)),
-                () -> System.out.println("Произошла ошибка при обработке массива"));
-    }
-
+    
 }
